dcf7829421b48ba48a3b80bd530f997c7e6c2e7b
Ty <6323925+Ty-lerCox@users.noreply.github.com>
2025-09-23 08:51:50 -0400

    asdf

---
+++ scripts/run.bat
@@ -0,0 +1,3 @@
+.\.venv\Scripts\activate
+
+python .\scripts\find_and_click.py --continuous ".\scripts\easy apply lightning.png" ".\scripts\easy apply.png" ".\scripts\go to search.png" ".\scripts\next.png" ".\scripts\submit.png" --confidence=0.9 --close-window ".\scripts\go to search.png"
